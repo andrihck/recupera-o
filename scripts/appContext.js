f0f9240c74c01252f8a23c698c112c3f23405619
Professor <Professor@SN-398464.edu.fiesc.com.br>
2024-10-10 14:48:57 -0300

    msg

---
+++ scripts/appContext.js
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext()
+export const AppProvider = ({ children }) => {
+    const [cidade, setCidade] = useState('')
+
+    return (
+        <AppContext.Provider value={{cidade, setCidade}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
